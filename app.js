4d5a206da03de841c69eec365dfff5c999bc809f
Andric Tham <andric@nuisance.co>
2017-06-09 12:06:15 +0800

    Set up Firebase

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ let twitterClient = new Twitter({
   bearer_token: process.env.TWITTER_BEARER_TOKEN
 })
 
+let firebaseServiceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
+}
+
 let getProfileInfo = function(handle) {
   twitterClient.get('users/show', { 'screen_name': handle }, function (error, response) {
     if (!error) {
